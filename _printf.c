450d4a21b859c5bf87078f1a1544e1b1ea327476
JennyHadir <hadirjenny@hotmail.com>
2020-10-26 22:15:42 +0100

    task

--- _printf.c
+++ _printf.c
@@ -12,3 +12,17 @@ type_printer frm[] ={
 {"c", print_c}
 {"s", print_s}
 };
+va_list arg;
+int i = 0;
+if (!format || (format[i] == "%" && format[i + 1] == '\0'))
+return (-1);
+va_start(arg, format);
+while(format && format[i])
+{
+if (frm[i].c == format[i])
+frm[i].f(arg);
+break;
+i++;
+}
+return(0);
+}
