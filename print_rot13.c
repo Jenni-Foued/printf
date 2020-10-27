dcbc8cae521eeb7dd0450a39168267b06c926631
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-27 18:47:00 +0100

    update

--- print_rot13.c
+++ print_rot13.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdarg.h>
+#include <stdlib.h>
 /**
  * print_rot13 - Prints a string in rot13.
  * @arg: String address.
@@ -8,13 +9,16 @@
 int print_rot13(va_list arg)
 {
 	int i, j, count = 0;
-	char a[] = "AaBbCcDdEeFfGgHhIiJjKkLlMm";
-	char b[] = "NnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char *s = va_arg(arg, char *);
+
+	if (!s)
+		s = "empty";
 
 	for (i = 0; s[i]; i++)
 	{
-		for (j = 0; j < 26; j++)
+		for (j = 0; j < 52; j++)
 		{
 			if (s[i] == a[j])
 			{
