638866e3614e9613fb86f1e7572d2619cd12bc22
JennyHadir <hadirjenny@hotmail.com>
2020-10-30 22:14:52 +0100

    update

--- print_rot13.c
+++ print_rot13.c
@@ -9,28 +9,29 @@
 int print_rot13(va_list arg)
 {
 	int i, j, count = 0;
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	char *s = va_arg(arg, char *);
 
-	if (!s)
-		s = "empty";
-
-	for (i = 0; s[i]; i++)
-	{
-		for (j = 0; j < 52; j++)
-		{
-			if (s[i] == a[j])
-			{
-				_putchar(b[j]);
-				count++;
-			}
-			else if (s[i] == b[j])
-			{
-				_putchar(a[j]);
-				count++;
-			}
-		}
-	}
-	return (count);
-	}
+if (s == NULL)
+s = "(NULL)";
+for (i = 0; s[i]; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (s[i] == a[j])
+break;
+}
+if (s[i] == a[j])
+{
+_putchar(b[j]);
+count++;
+}
+else
+{
+_putchar(s[i]);
+count++;
+}
+}
+return (count);
+}
