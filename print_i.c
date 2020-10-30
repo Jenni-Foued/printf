c96998cb164952b61e43eb94e3ca84c6648dee10
Jenni-Foued <medfouedjenni@gmail.com>
2020-10-30 20:52:29 +0100

    reverted push

--- print_i.c
+++ print_i.c
@@ -7,7 +7,7 @@
 int print_i(va_list arg)
 {
 	int ar = va_arg(arg, int);
-	unsigned int count = 0, i = 0, ar2 = 0, ar3, j = 1, j1;
+	unsigned int count = 0, i = 0, ar2 = 0, ar3, j = 1;
 
 	if (ar < 0)
 	{
@@ -27,14 +27,11 @@ int print_i(va_list arg)
 	i--;
 	for (j = 1; i; i--)
 		j *= 10;
-	j1 = j;
-	_putchar((ar3 / j) + '0');
-	count++;
-	j /= 10;
-	for (i = 10; i <= j1; i *= 10)
+	for (i = 0; ar3; i++)
 	{
-		_putchar(((ar3 / j) % i) + '0');
+		_putchar(((ar3 / j) % 10) + '0');
 		count++;
+		ar3 %= j;
 		j /= 10;
 	}
 	}
